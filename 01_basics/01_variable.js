c65fd55a5836c75ea1b35dc67086e2715df2ae32
Nirmal Solanki <125039492+nirmalsolanki30@users.noreply.github.com>
2023-11-03 16:43:33 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const num=1;
+let email="solanki@gmail.com";
+var myname="Nirmal";
+city="Rajkot";
+console.table([num,email,myname,city]);
+/*prefer not to use var
+because of issue in block scope and functional scope */
